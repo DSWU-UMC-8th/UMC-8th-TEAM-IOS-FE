3faca1ce3113d29e69540b88a8d56062df0f6ed5
김건 <gimgeon@gimgeon-ui-MacBookAir-2.local>
2025-05-24 19:12:42 +0900

    Feat: 마이 뷰 레이아웃 구현

---
+++ UMC8thMovieIn/Models/ReviewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ReviewModel.swift
+//  UMC8thMovieIn
+//
+//  Created by 신연주 on 5/23/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct ReviewModel {
+    let movieTiTle: String
+    let movieImage: String
+    let movieRating: Int
+    let movieReview: String 
+}
