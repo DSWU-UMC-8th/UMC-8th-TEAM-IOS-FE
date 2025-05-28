51ae555676b894bb9da7549e2346cb9c4c26a22d
mzxxzysy <jsyjj21@gmail.com>
2025-05-28 18:29:04 +0900

    Feat: 리뷰 작성 페이지 뷰

---
+++ UMC8thMovieIn/Models/PostModel.swift
@@ -0,0 +1,14 @@
+//
+//  PostModel.swift
+//  UMC8thMovieIn
+//
+//  Created by 정서영 on 5/28/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct PostModel {
+    let rating: Int
+    let content: String
+}
---
+++ UMC8thMovieIn/ViewModels/PostViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  PostViewModel.swift
+//  UMC8thMovieIn
+//
+//  Created by 정서영 on 5/28/25.
+//
+
+import Foundation
+
+@Observable
+
+class PostViewModel {
+    
+}
